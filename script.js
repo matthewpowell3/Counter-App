6faf6e9e246a0f3f18cfaab72e147a7f5748ad69
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-08 01:36:39 -0500

    3 files modified

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ let number = 0;
 function counterFun() {
   number++;
   document.getElementById('number').innerHTML = number;
+  document.getElementById('number').style.color = 'lightgreen';
+}
+
+function resetFun() {
+  number = 0;
+  document.getElementById('number').innerHTML = number;
+  document.getElementById('number').style.color = 'black';
+}
+
+function subtractFun() {
+  number--;
+  document.getElementById('number').innerHTML = number;
+  document.getElementById('number').style.color = 'red';
 }
